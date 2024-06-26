f98475ffbc607141460403a101f0ebe740776916
Akash Morwal <akashbhau101@gmail.com>
2024-06-26 19:46:58 +0530

    new factorial program is updated

---
+++ FindFactorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//FactorialProgram.
+
+class FindFactorial{
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+
+		int result  = 1;
+		System.out.println("Enter a Number");
+        int num = input.nextInt();
+        for(int i=2;i<=num;i++)
+        {
+        	result *=i;
+        }
+        
+        System.out.println("Factorial of "+num+" is "+result); 
+	}
+}
