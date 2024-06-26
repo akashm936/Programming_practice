1a44b7c11c9ab9137e47c659c2dc53cf702c5cab
Akash Morwal <akashbhau101@gmail.com>
2024-06-27 01:33:05 +0530

    new program of Finding Even number is updated (27-06-2024)

---
+++ EvenNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class EvenNumber
+{
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("enter first Number : ");
+		int num1 = input.nextInt();
+
+		System.out.println("enter second Number : ");
+		int num2 = input.nextInt();
+
+		for(int i=num1;i<=num2;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(i+ " ");
+			}
+		}
+
+	}
+}
