36dae86aa2c8739d8c3c82b638cf281bd62c1222
Akash Morwal <akashbhau101@gmail.com>
2024-06-27 11:42:08 +0530

    new program is updated (27-06-2024)

---
+++ Quetion1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Quetion1
+{
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+        int numberChapter = input.nextInt();
+        int b= input.nextInt();
+        int e = input.nextInt();
+        int []arr ={};
+        int op =0;
+        for(int i=0;i<=numberChapter;i++)
+        {
+        	arr[i]=input.nextInt();
+        	//if(arr[numberChapter])
+        }
+
+        for(int i=b;i<=e;i++){
+              op=arr[i];
+        }
+        System.out.println(op);
+	}
+}
