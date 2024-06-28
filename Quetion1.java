aec0ee6e45a42626c88ac2e127e76e99506a6096
Akash Morwal <akashbhau101@gmail.com>
2024-06-28 23:40:48 +0530

    new program is updated (28-06-2024)

--- Quetion1.java
+++ Quetion1.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 // run status : 
 
 
@@ -8,5 +11,17 @@ public class Quetion1 {
 
       public static void main(String[] args) {
             
+            int num1 , num2;
+
+            Scanner input = new Scanner(System.in);
+            num1 = input.nextInt();
+            num2 = input.nextInt();
+            if(num1 >num2)
+            {
+                  System.out.println(num1);
+            }
+            else{
+                  System.out.println(num2);
+            }
       }
 }
