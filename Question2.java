8496fa11e247e85092f3c4c9184a5a60026f45be
Akash Morwal <akashbhau101@gmail.com>
2024-06-29 21:54:37 +0530

    new program is updated (29-06-2024)

---
+++ Question2.java
@@ -0,0 +1,20 @@
+// write a program to find the factor for a given natural numbers.
+// test case 1:
+// input :0
+// output: 1,3,9
+
+// test case 2:
+// input :15
+// ouput: 1,3,5,15;
+
+import  java.util.Scanner;
+class Question2
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        System.out.println(1 + ", ");
+   }
+}
