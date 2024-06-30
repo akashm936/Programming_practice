7306a056885baf75ea78e0603f09a6ee5d9519cf
Akash Morwal <akashbhau101@gmail.com>
2024-06-30 23:46:07 +0530

    new program is updated (30-06-2024)

--- Program7.java
+++ Program7.java
@@ -4,5 +4,8 @@ class program7
 	public static void main(String[] args) {
 		
 		double max_temp = 80.0 , min_temp = 70;
+	    Scanner sc = new Scanner(System.in);
+
+	    double cur_temp = sc.nextDouble(); 
 	}
 }
