3d82d92c67535b14c8c196e57b178fddc815e771
Akash Morwal <akashbhau101@gmail.com>
2024-06-30 23:48:16 +0530

    new program is updated (30-06-2024)

--- Program7.java
+++ Program7.java
@@ -10,6 +10,10 @@ class program7
 	     cur_temp = cur_temp*(9.0/5)+32;
 
 	     System.out.println(cur_temp);
-
+         
+         if(cur_temp >= min_temp && cur_temp <= max_temp)
+         {
+         	System.out.println("temperature is within the range");
+         }
 	}
 }
