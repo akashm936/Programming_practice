c241e91ba7278ea9c9b577396805f1fd07a47677
Akash Morwal <akashbhau101@gmail.com>
2024-07-09 01:05:30 +0530

    new program is updated (09-07-2024)

---
+++ PrintFExample.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class PrintFExample
+{
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("==========================");
+        for(int i=0;i<3;i++)
+        {
+        	String input1 = scanner.next();
+        	int input2 = scanner.nextInt();
+        	System.out.printf(" %-14s , %03d\n",input1,input2);
+        }
+        System.out.println("===========================");
+	}	
+}
