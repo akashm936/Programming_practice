f114e274344f131885338b012f13c21da58b80a2
Akash Morwal <akashbhau101@gmail.com>
2024-07-10 00:34:33 +0530

    new program is updated (10-07-2024)

---
+++ MergerTwoArray.java
@@ -0,0 +1,33 @@
+// import java.util.Scanner;
+import java.util.Arrays;
+
+class MergerTwoArray
+{
+	public static void main(String[] args) {
+		
+		int [][] arr = {{10,20,30},{40,50,60,70},{80,90}};
+
+		//to get the length of 2d array
+
+		int len =0;
+		for(int[]i:arr)
+		{
+			len +=arr.length;
+		}
+		//new Array created using length
+
+		int [] arr1 = new int[len];
+
+		int indx =0;
+		// iterating 2d array using for each and int i element
+
+		for(int []i:arr)
+		{
+			for(int j: i)
+			{
+				arr1[indx++]=j;
+			}
+		}
+		System.out.println(Arrays.toString(arr1));
+	}
+}
