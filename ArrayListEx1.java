4ec248dbee9c2620fc24016c363b7083051ed428
Akash Morwal <akashbhau101@gmail.com>
2024-07-14 12:48:52 +0530

    new program is upated (14-07-2024)

---
+++ ArrayListEx1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+class ArrayListEx1
+{
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> arrayList = new ArrayList<>();
+
+		arrayList.add(20);
+		arrayList.add(30);
+		arrayList.add(40);
+		arrayList.add(70);
+		arrayList.add(80);
+
+		System.out.println(arrayList); 
+	}
+}
