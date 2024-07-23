61360a00f8cc7865999dd976b3213fbc4cc6eb04
Akash Morwal <akashbhau101@gmail.com>
2024-07-23 23:54:10 +0530

    new program is updated (23-07-2024)

--- ArrayAssignment1.java
+++ ArrayAssignment1.java
@@ -1,13 +1,26 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-class AssessmentArray
+// this program is used to find the gretest element from array
+
+class ArrayAssignment1
 {
 	public static void main(String[] args) {
 		
 		int[]a={16,17,4,3,5,2};
 		System.out.println(Arrays.toString(a));
 	int max = Integer.MIN_VALUE;
-	int
+	int idx = 0;
+
+	for(int i=0;i<a.length;i++)
+	{
+		if(max<a[i])
+		{
+			idx =i;
+			max =a[i];
+		}
+	}
+
+	System.out.println(max);
 	}
 }
