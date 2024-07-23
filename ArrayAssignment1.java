d42d586f57f680b969a2ebf46e6894625f04e88c
Akash Morwal <akashbhau101@gmail.com>
2024-07-23 23:49:36 +0530

    New program of Array is Updated (23-07-2024)

---
+++ ArrayAssignment1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class AssessmentArray
+{
+	public static void main(String[] args) {
+		
+		int[]a={16,17,4,3,5,2};
+		System.out.println(Arrays.toString(a));
+	int max = Integer.MIN_VALUE;
+	int
+	}
+}
